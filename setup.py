53b9a13e5712123c9613c9ae3c70963ee154bdb4
Matt Kollada <mattkollada@gmail.com>
2021-10-25 15:10:42 -0400

    removing strict version requirement for gym

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='swell',
       version='0.0.1',
-      install_requires=['gym==0.21.0',
+      install_requires=['gym',
                         'seaborn==0.11.1',
                         'pygame==2.0.1',
                         'numpy==1.20.3',]
